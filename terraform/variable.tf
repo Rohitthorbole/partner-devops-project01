0bba863b3a18b6f2237217ab5166cb5584e5dd23
Rohit Thorbole <rohitthorbole4@gmail.com>
2026-08-29 18:05:53 +0530

    configuring providers

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,3 +1,11 @@
+#Common variables
+variable "aws_region" {
+  description = "AWS region for resources"
+  type = string
+  default = "us-east-1"
+}
+
+#S3 bucket variables
 variable "bucket_name" {
   description = "The name of the S3 bucket to create"
   type        = string
